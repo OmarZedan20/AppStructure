aaf555b9413a816806552eb2033e08fb5a4e7a11
omarzedan20 <omarzcb@gmail.com>
2024-08-30 23:39:21 +0300

    FirstDemo ( MVC )

---
+++ AppStructure/Controllers/MovieController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppStructure.Controllers
+{
+    public class MovieController:Controller 
+    {
+        public void GetMovie(int id) 
+        {
+
+            
+        }
+    }
+}
--- AppStructure/Program.cs
+++ AppStructure/Program.cs
@@ -1,15 +1,28 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace AppStructure
 {
     public class Program
     {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("{controller}/{action}/{id?}", () => "Hello World!");
 
             app.Run();
+            
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action}/{id:int?}/{name:alpha?}"
+
+                );
+            
         }
     }
 }
